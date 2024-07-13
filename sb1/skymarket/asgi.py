b4d13030c066ddb36c21e95a259d7fdaf6759d93
Alexander <san.juckov2017@yandex.ru>
2024-07-13 18:10:32 +0300

    Changing the project name

--- sb1/skymarket/asgi.py
+++ sb1/skymarket/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for skymarket project.
+ASGI config for sb1 project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- sb1/skymarket/wsgi.py
+++ sb1/skymarket/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for skymarket project.
+WSGI config for sb1 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
